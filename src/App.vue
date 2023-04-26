4a140ae5f8313f26fc0305ae0b69c2568d950aa7
Jude Harvey Maldicas <jude.maldicas@betterask.erni>
2023-04-26 11:18:16 +0800

    Add bootstrap for css

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
+  <Navbar />
   <router-view />
 </template>
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
+html,
+body {
+  font-family: "Open Sans", sans serif;
+  height: 100%;
+  margin: 0;
+}
+
+/* #nav {
   padding: 30px;
 }
 
@@ -26,5 +38,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
